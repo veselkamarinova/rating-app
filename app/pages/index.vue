1e7f7a2b7e7c6365246122f7ba1affc4fc2db08e
Veselka Marinova <vsl.marinova@gmail.com>
2025-11-27 17:44:52 +0200

    Add array of wines.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <h1>Wine Test</h1>
-    <p>Label: {{myWine.label }}</p>
-    <p>typeWine: {{ myWine.typeWine }}</p>
-    <p>Grape: {{ myWine.grapeVarietal }}</p>
-    <p>Vintage: {{ myWine.vintage }}</p>
-    <p>Winery: {{ myWine.winery }}</p>
-    <p>Price: {{ myWine.price }} EUR</p>
-    <p>Rating: {{ getStars(myWine.rating) }}</p>
+    <h1>My Wine Collection</h1>
+    <div v-for="wine in wines" :key="wine.label">
+      <h2>{{ wine.label }}</h2>
+      <p>Type: {{ wine.typeWine }}</p>
+      <p>Grape: {{ wine.grapeVarietal }}</p>
+      <p>Vintage: {{ wine.vintage }}</p>
+      <p>Winery: {{ wine.winery }}</p>
+      <p>Price: {{ wine.price }} EUR</p>
+      <p>Rating: {{ getStars(wine.rating) }}</p>
+      <hr>
+    </div>
   </div>
 </template>
 
@@ -15,21 +18,48 @@
 import { Wine } from '~/models/Wine';
 import { WineType } from '~/models/enums';
 
-const myWine = new Wine(
-  18.87,                    // price
-  'Vila Melnik',            // store
-  'wine',                   // typeProduct
-  'Aplauz',                 // label
-  WineType.Red,             // typeWine - Use enum!
-  'Melnik 55',              // grapeVarietal
-  'Vila Melnik',            // winery
-  'Bulgaria',               // appellation
-  2021                      // vintage
-);
-// Rate the wine!
-myWine.setRating(4);
+// Create an array of wines
+const wines: Wine[] = [
+  new Wine(
+    18.87,
+    'Vila Melnik',
+    'wine',
+    'Aplauz',
+    WineType.Red,
+    'Melnik 55',
+    'Vila Melnik',
+    'Bulgaria',
+    2021
+  ),
+  new Wine(
+    25.99,
+    'Wine Shop Sofia',
+    'wine',
+    'Château Margaux',
+    WineType.Red,
+    'Cabernet Sauvignon',
+    'Château Margaux',
+    'Bordeaux',
+    2018
+  ),
+  new Wine(
+    15.50,
+    'Local Store',
+    'wine',
+    'Chardonnay Reserve',
+    WineType.White,
+    'Chardonnay',
+    'Domaine Leflaive',
+    'Burgundy',
+    2020
+  )
+];
 
-// Function to display stars
+// Rate the wines
+wines[0]?.setRating(5);
+wines[1]?.setRating(4);
+wines[2]?.setRating(3);
+
 const getStars = (rating: number): string => {
   return '⭐'.repeat(rating) + '☆'.repeat(5 - rating);
 };
